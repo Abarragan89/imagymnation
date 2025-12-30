c567ecc3b61612d39ec4867be63f83070ce12240
Anthony Barragan <anthony.bar.89@gmail.com>
2025-12-30 15:05:33 -0800

    UPDATE: Added the why choose us section cards

--- src/data/image-urls.ts
+++ src/data/image-urls.ts
@@ -1,8 +1,12 @@
 export const imageUrls = [
     "https://nebula.wsimg.com/b36c3e3b15af615a1a841962fc702bc9?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
     "https://nebula.wsimg.com/0b18934dc0de1ceddc824a00dc6c6980?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
+    
+    // kid hangingn on rope
     "https://nebula.wsimg.com/339dfe8b8be74706019eeee6b00a053e?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
     "https://nebula.wsimg.com/f5cfcb94a5f64198c7fa308128bf16b6?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
+    
+    // Kid holding an waward.
     "https://nebula.wsimg.com/f075b82ea70ba78d17cebbf5da9a645c?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
     "https://nebula.wsimg.com/3f441f3508f12fa8f0c942c4568723a1?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
     "https://nebula.wsimg.com/3dc1a8d84a468b42bb48e86df32c37be?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
@@ -10,12 +14,19 @@ export const imageUrls = [
     "https://nebula.wsimg.com/6da0987f7b31185b09913fe05650aebe?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
     "https://nebula.wsimg.com/65623ed739b4d5ce4efe815cc4e4f7e7?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
     "https://nebula.wsimg.com/2574857b9d37ffbd5024c90e0c87d1f4?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
+    
+    // Kid pyramid
     "https://nebula.wsimg.com/d5f9c57266944b86109b00959ae43ba7?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
     "https://nebula.wsimg.com/bd3a5b6dfe5204f7276e01c56c233cec?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
+    
+    // holding a handstand agains wall
     "https://nebula.wsimg.com/e0cb5818d62aa17fdc306545d43719a5?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
     "https://nebula.wsimg.com/f14d9b048e6c64f4e2c1b475eb7179f5?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
     "https://nebula.wsimg.com/438bd0b433847aaa074295269c4edc3d?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
+    
+    // Kids holding upd americna flag hamds
     "https://nebula.wsimg.com/2678485819ffc11e22b46873e1b7501e?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
+    
     "https://nebula.wsimg.com/15e20398909570f50c218c8d101b9995?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
     "https://nebula.wsimg.com/9665f6a224d25be3a287cdae107345fb?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
     "https://nebula.wsimg.com/60053f683902cefbcdcb020c53a0e911?AccessKeyId=E939DB3FED0800A88125&disposition=0&alloworigin=1",
