ff977c7bd9c7ac7029d9e799451ed8e00635640c
Anthony Barragan <anthony.bar.89@gmail.com>
2026-01-02 21:32:55 -0800

    added qr code to website

--- src/components/login-button.tsx
+++ src/components/login-button.tsx
@@ -19,7 +19,7 @@ export default function LoginButton({isRounded = false}: {isRounded?: boolean}) 
                     <DialogTitle className="text-2xl font-bold mb-2">Customer Portal</DialogTitle>
                 </DialogHeader>
                 <div className="max-w-87.5 mx-auto w-full flex flex-col">
-                    <Button variant={"accent"} className="text-lg font-medium mb-7">
+                    <Button asChild variant={"accent"} className="text-lg font-medium mb-7">
                         <a href="https://app2.jackrabbitclass.com/jr3.0/ParentPortal/Login?orgId=507391" target="_blank" rel="noopener noreferrer">Customer Login</a>
                     </Button>
                     <p className="text-center font-bold text-lg pb-2 pt-3 border-t border-black/10">Need an Account?</p>
